47b6d1c94096c505e02d9154cb2381a9c383f626
kulak <kulakova.tatyana12@gmail.com>
2023-04-12 21:25:58 +0300

    fix enabled CORS - 2

--- config/cors.php
+++ config/cors.php
@@ -15,9 +15,9 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://example.com'],
+    'paths' => ['api/*', '*'],
+    'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', '*'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
